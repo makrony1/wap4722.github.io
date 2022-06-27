3bd8e56627df1614153004d315858933e744941d
Abdul Kadir <akadir@wcgclinical.com>
2022-06-26 19:31:18 -0500

    lab 5 completed.

--- Lab 5/main.js
+++ Lab 5/main.js
@@ -1,8 +1,22 @@
-console.log('helllo');
-var a=[10,20,3,0];
-var result = a.reduce((pre, cur, index, array)=>{
-    console.log("p"+pre);
-    console.log("c"+cur);
-    return pre+cur;
-},0);
-console.log(result);
+//Create a function using function declaration named sum with one parameter of Array type, the
+//returned result is the sum of all elements which are greater than 20.
+
+const numbers=[20,1,30,40,10,40,5,35];
+function sum(numbers){
+   return numbers.filter(number=>number>20).reduce((accum,number)=>accum+number,0);
+}
+console.log("The sumer of all elements which are greater than 20 is:: "+sum(numbers));
+
+const sum2=(arr)=>arr.filter(x=>x>20).reduce((previous,current,index,array)=>previous+current,0);
+
+console.log(sum2(numbers));
+// Create a function using function expression named getNewArray with one parameter of String
+// Array, return a new array which contains all string, length is greater than and equal to 5, and
+// contains letter ‘a’.
+
+const strings=["Anil","Sachin","Ashish","Shree","Sayal","Siddhant"];
+const getNewArray=(strings)=>{
+    return strings.filter(item=>item.length>5 && item.includes('a')).map(str=>str);
+}
+console.log(getNewArray(strings));
+
